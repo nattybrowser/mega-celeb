b719a3ad508019099f06fdc409e50eeea6b5a7b9
nattybrowser <nattyskabshe@gmail.com>
2024-07-20 04:27:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -22,6 +22,20 @@ async function fetchPosts() {
 
 function displayPosts() {
   const postBatch = posts.slice(currentIndex, currentIndex + 21);
+
+  // Check for embed in each post
+  const postsWithEmbed = postBatch.filter(post => post.embed);
+
+  // If there are embeds, display them first
+  if (postsWithEmbed.length > 0) {
+    postsWithEmbed.forEach(post => {
+      const embedElement = createEmbedElement(post.embed);
+      postsContainer.appendChild(embedElement);
+    });
+  }
+
+
+    
   postBatch.forEach(post => {
     const postDiv = document.createElement('div');
     postDiv.className = 'post';
